69d0eda4ca6aa6d207025285e31f8afb7348e847
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-06 15:46:17 +0530

    Added: Welcome message to banking simulator using stack.

---
+++ BankingSimulatorStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingSimulatorStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to banking simulator using Stack list.");
+        }
+    }
+}
