36a074817cf4c129c36be3a4a9f3f152b285fb89
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-06 16:04:15 +0530

    Added: cash withdraw and cash deposite logic done.

---
+++ BankingSimulatorStack/Banking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankingSimulatorStack
+{
+    // cash withdraw and deposite are done
+    public class Banking
+    {
+        // cash withdraw
+        public int cashWithdraw(int cashToWithdraw, int currentBalance)
+        {
+            currentBalance -= cashToWithdraw;
+            return currentBalance;
+        }
+
+        // cash deposite
+        public int cashDeposite(int cashToDeposite, int currentBalance)
+        {
+            currentBalance += cashToDeposite;
+            return currentBalance;
+        }
+    }    
+}
